c955e1a9faa3131e7b778baba512635c30f0e5c5
jacobjuarezguerra <72465700+jacobjuarezguerra@users.noreply.github.com>
2020-10-06 13:10:07 -0600

    ere

--- server.js
+++ server.js
@@ -1,27 +1,3 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-require('events').EventEmitter.defaultMaxListeners = 100;
-//
-app.use(express.static('public'));
-
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-
-app.listen(process.env.PORT);
-
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`); 
-}, 280000);
-
-
-
-///////////////////////////////////BOT REAL//////////////////////////////////////////////
 
 const Discord = require("discord.js");
 
