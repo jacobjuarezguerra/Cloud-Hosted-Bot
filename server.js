373de3b759e00a85e4dc0087aacffc5fec265619
jacobjuarezguerra <72465700+jacobjuarezguerra@users.noreply.github.com>
2021-04-30 09:11:29 -0600

    Revert "Revert "Update server.js""
    This reverts commit 43ef1a7e2a051c3ee577871ece3d1bb4f906a138.

--- server.js
+++ server.js
@@ -587,4 +587,11 @@ client.on("message", (message) => {
     }
 
 })
+client.on("message", (message) => {
+    if(message.content.startsWith("Bayer")) {
+        message.channel.send("That's it stop rigth now! ")
+    }
+
+})
+
 client.login(process.env.token);
